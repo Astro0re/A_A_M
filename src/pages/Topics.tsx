803812289e8773e3cfb5bacd18fe804e66d5a9a0
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-11-01 15:05:52 +0100

    update

---
+++ src/pages/Topics.tsx
@@ -0,0 +1,12 @@
+export default function topics() {
+    return (
+        <>
+            <div>
+                <h1>Topics</h1>
+                <p>Confused, don't know what to do?</p>
+                <p>Don't worry we don't either, so get in here and help us figure this our</p>
+                <p>Chat like function where polls are droped periodicaly on the subject matter by the topic creator and admins</p>
+            </div>        
+        </>
+    )
+} 
